daed7bdc59c979befa36196edfc442925b5fe4f5
Imane Attif <imaneattif.95@gmail.com>
2024-03-13 01:18:15 -0400

    Added webpack and made code more modular

--- api/scripts/trymongo.js
+++ api/scripts/trymongo.js
@@ -7,35 +7,36 @@ const url = process.env.DB_URL || 'mongodb://localhost/cop2836';
 function testWithCallbacks (callback) {
   console.log('\n--- testWithCallbacks ---');
   const client = new MongoClient(url, { useNewUrlParser: true });
-  client.connect(function(err, client) {
-    if (err) {
-      callback(err);
+  client.connect((connErr) => {
+    if (connErr) {
+      callback(connError);
       return;
     }
-    console.log('Connected to MongoDB');
+
+    console.log('Connected to MongoDB', url);
 
     const db = client.db();
 
     const collection = db.collection('employees');
 
     const employee = { id: 1, name: 'A. Callback', age: 23 };
-    collection.insertOne(employee, function(err, result) {
-      if (err) {
+    collection.insertOne(employee, (insertErr, result) => {
+      if (insertErr) {
         client.close();
-        callback(err);
+        callback (insertErr);
         return;
       }
       console.log('Result of insert:\n', result.insertedId);
       collection.find({ _id: result.insertedId})
-        .toArray(function(err, docs) {
-          if (err) {
+        .toArray((findErr, docs) => {
+          if (findErr) {
             client.close();
-            callback(err);
+            callback(findErr);
             return;
           }
           console.log('Result of find:\n', docs);
           client.close();
-          callback(err);
+          callback();
         });
     });
   });
